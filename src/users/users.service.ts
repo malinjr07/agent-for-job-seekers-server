42d64876b2b30d3e54f1d51773a2fa49babfe2ea
malinjr07 <malinjr07@gmail.com>
2025-11-26 18:59:04 +0600

    basic module covered

---
+++ src/users/users.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: CreateUserDto) {
+    return this.prisma.user.create({ data });
+  }
+}
