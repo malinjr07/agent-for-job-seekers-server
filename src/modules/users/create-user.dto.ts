dc34c170270acb14d344bb48de8720f4583f0d6a
malinjr07 <malinjr07@gmail.com>
2025-11-28 01:11:05 +0600

    user payload validation done

--- src/modules/users/create-user.dto.ts
+++ src/modules/users/create-user.dto.ts
@@ -4,6 +4,7 @@ import {
   IsString,
   Length,
   Matches,
+  IsStrongPassword,
 } from 'class-validator';
 
 export class CreateUserDto {
@@ -15,10 +16,19 @@ export class CreateUserDto {
   @IsString()
   @IsNotEmpty({ message: 'Password is required' })
   @Length(10, 16, { message: 'Password must be between 10 and 16 characters' })
-  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/, {
-    message:
-      'Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character',
-  })
+  @IsStrongPassword(
+    {
+      minLength: 10,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    },
+    {
+      message:
+        'Password must contain at least 1 uppercase letter, 1 lowercase letter, 1 number, and 1 special character',
+    },
+  )
   @Matches(/^[\x20-\x7E]+$/, { message: 'Password must not contain emojis' })
   password: string;
 
