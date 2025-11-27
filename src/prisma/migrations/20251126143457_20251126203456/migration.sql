137f133a5f2aaf999bcb9413085d24c51cf3e026
malinjr07 <malinjr07@gmail.com>
2025-11-27 14:15:47 +0600

    schema develop completed

--- src/prisma/migrations/20251126143457_20251126203456/migration.sql
+++ src/prisma/migrations/20251126143457_20251126203456/migration.sql
@@ -1,34 +1,21 @@
-/*
-  Warnings:
-
-  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
-  - You are about to alter the column `email` on the `User` table. The data in that column could be lost. The data in that column will be cast from `Text` to `VarChar(255)`.
-  - Added the required column `firstName` to the `User` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `lastName` to the `User` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `passwordHash` to the `User` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
-  - Changed the type of `id` on the `User` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
-
-*/
 -- CreateEnum
 CREATE TYPE "MailStatus" AS ENUM ('SENT', 'FAILED', 'PENDING');
 
 -- CreateEnum
 CREATE TYPE "FlagStatus" AS ENUM ('GREEN', 'YELLOW', 'RED');
 
--- AlterTable
-ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
-DROP COLUMN "name",
-ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-ADD COLUMN     "firstName" VARCHAR(100) NOT NULL,
-ADD COLUMN     "lastName" VARCHAR(100) NOT NULL,
-ADD COLUMN     "passwordHash" VARCHAR(255) NOT NULL,
-ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL,
-DROP COLUMN "id",
-ADD COLUMN     "id" UUID NOT NULL,
-ALTER COLUMN "email" SET DATA TYPE VARCHAR(255),
-ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+-- CreateTable
+CREATE TABLE "User" (
+    "id" UUID NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "passwordHash" VARCHAR(255) NOT NULL,
+    "firstName" VARCHAR(100) NOT NULL,
+    "lastName" VARCHAR(100) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
 
 -- CreateTable
 CREATE TABLE "GoogleSheet" (
@@ -126,6 +113,9 @@ CREATE TABLE "EmailFlag" (
 
     CONSTRAINT "EmailFlag_pkey" PRIMARY KEY ("id")
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "EmailFlag_email_key" ON "EmailFlag"("email");
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto } from './create-user.dto';
 
 @Injectable()
 export class UsersService {
