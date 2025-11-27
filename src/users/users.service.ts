9f4f7b40a040e4ecce167e16fed8089e19bb28a7
malinjr07 <malinjr07@gmail.com>
2025-11-27 23:46:17 +0600

    App run with basic Reest API

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,42 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
+import { PrismaService } from '@prisma/prisma.service';
+import type { User } from '@prisma/client';
 import { CreateUserDto } from './create-user.dto';
+import { hash } from 'argon2';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async create(data: CreateUserDto) {
-    return this.prisma.user.create({ data });
+  async create({
+    email,
+    password,
+    firstName,
+    lastName,
+  }: CreateUserDto): Promise<User> {
+    console.log('Payload', {
+      email,
+      password,
+      firstName,
+      lastName,
+    });
+
+    const passwordHash = await hash(password, {
+      timeCost: 3,
+      memoryCost: 12288, // 12 MB
+      parallelism: 1,
+      type: 2,
+    });
+
+    console.log('passwordHash', passwordHash);
+
+    return this.prisma.user.create({
+      data: {
+        email,
+        passwordHash,
+        firstName,
+        lastName,
+      },
+    });
   }
 }
